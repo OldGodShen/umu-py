5df35e5253212c9e5956b6c27369c1c9625ebd4b
OldGodShen <2415479434@qq.com>
2023-03-08 17:36:23 +0800

    add answertext

--- fun.py
+++ fun.py
@@ -66,6 +66,34 @@ def getanswer(element_id,question_ids):
             answer.append({"type":"input","question_id":int(rightanswer_key),"answer_ids":"[]","content":"" + content + "","level":2})
     answer_json = json.dumps(answer)
     return answer_json
+
+def getanswertext(answertext):
+
+    response_dict = json.loads(answertext)
+
+    rightanswer_data = response_dict['data']
+
+    answer = []
+    for rightanswer_key,rightanswer_value in rightanswer_data.items():
+        for rightanswer_values_type,rightanswer_values_right in rightanswer_value[0].items():
+            answer_ids = []
+            if rightanswer_values_type == "right_answer_id":
+                answer_ids.append(rightanswer_values_right)
+                if len(rightanswer_value[0]) == 1:
+                    answer_type = "radio"
+                else:
+                    answer_type = "checkbox"
+            elif rightanswer_values_type == "right_answer_content":
+                content = rightanswer_values_right
+                answer_type = "input"
+        if answer_type == "radio":
+            answer.append({"type":"radio","question_id":int(rightanswer_key),"answer_ids":answer_ids,"content":"","level":2})
+        elif answer_type == "checkbox":
+            answer.append({"type":"checkbox","question_id":int(rightanswer_key),"answer_ids":answer_ids,"content":"","level":2})
+        elif answer_type == "input":
+            answer.append({"type":"input","question_id":int(rightanswer_key),"answer_ids":"[]","content":"" + content + "","level":2})
+    answer_json = json.dumps(answer)
+    return answer_json
             
 def startexam(element_id,exam_submit_id,umuU,JSESSID):
     url = "https://m.umu.cn/megrez/exam/v1/startExam"
